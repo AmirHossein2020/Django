a60402b52b3ba6badcf4a080f722a0d34d705ee5
Amir Hossin <147377111+Amir20Hossein@users.noreply.github.com>
2024-01-25 16:20:18 +0330

    Delete blog directory

--- blog/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms 
-
-class CommentForm(forms.Form):
-    name = forms.CharField(max_length=50)
-    email = forms.EmailField(max_length=250)
-    message = forms.CharField(widget=forms.Textarea)
